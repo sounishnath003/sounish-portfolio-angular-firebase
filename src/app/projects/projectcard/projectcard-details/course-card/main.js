1deb1c72fae5fd842ef4f875b7fca2195b152ad5
Sounish Nath <flock.sinasini@gmail.com>
2020-06-07 01:34:18 +0530

    added UI

---
+++ src/app/projects/projectcard/projectcard-details/course-card/main.js
@@ -0,0 +1,7 @@
+$(".close-btn").click(() => {
+  $(".social-panel-container").removeClass("visible").addClass("visible");
+});
+
+$(".floating-btn").click(function () {
+  $(".social-panel-container").removeClass("visible").addClass("visible");
+});
