03caa43b0ba03a6120b4ca23cd1a5055235e33bc
Sounish Nath <flock.sinasini@gmail.com>
2020-06-08 12:04:58 +0530

    jquery fix

--- src/app/projects/projectcard/projectcard-details/course-card/course-card.component.ts
+++ src/app/projects/projectcard/projectcard-details/course-card/course-card.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  styleUrls: ['./course-card.component.css'],
 })
 export class CourseCardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    $(document).ready(() => {
+      this._toggleGetTouchButton();
+    });
   }
 
+  _toggleGetTouchButton() {
+    $('.close-btn').click(() => {
+      $('.social-panel-container').toggleClass('visible');
+    });
+
+    $('.floating-btn').click(function () {
+      $('.social-panel-container').removeClass('visible').addClass('visible');
+    });
+  }
 }
